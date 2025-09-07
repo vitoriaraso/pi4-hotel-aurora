57560bacf2b7b760fef7b426c44dae8a1ecd0e35
Dai <vitoriaraso01@gmail.com>
2025-09-07 13:48:07 -0300

    fix: sidenav fecha quando link é clicado

--- src/shared/sidenav.component/sidenav.component.ts
+++ src/shared/sidenav.component/sidenav.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatButtonModule, MatSidenavModule],
+  imports: [MatButtonModule, MatSidenavModule, RouterLink],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
+  @Output() linkClicked = new EventEmitter<void>();
 }
