f4ea0f06fbe8970e0c15f36386c4c43b00001ead
Dai <vitoriaraso01@gmail.com>
2025-10-03 01:54:37 -0300

    feat: página da conta to-do: mta coisa a fazer, corrigir o 'editar', colocar um sinal de que está no modo edição, conectar a API, mudar o 'salvar' pq só tá salvando seção individual ao invés de tudo

--- src/pages/account.component/account.component.ts
+++ src/pages/account.component/account.component.ts
@@ -11,5 +11,61 @@ import { ButtonComponent } from '../../shared/button.component/button.component'
 export class AccountComponent {
   name: string = "Zezinho Balboa";
   tel: string = "(00) 0000-0000";
-  email: string = "zezinho@corp.ze"
+  email: string = "zezinho@corp.ze";
+
+  cep: string = "000000";
+  logradouro: string = "000000";
+  numero: string = "000000";
+  complemento: string = "000000";
+  bairro: string = "Selecione";
+  localidade: string = "000000";
+  uf: string = "000000";
+
+  personalInfoButtonLabel: string = 'Editar';
+  isPersonalInfoEditing: boolean = false;
+
+  addressInfoButtonLabel: string = 'Editar';
+  isAddressInfoEditing: boolean = false;
+
+  togglePersonalInfo() {
+    const personalInfoInputs = document.querySelectorAll<HTMLInputElement>('.personal-info-input');
+
+    if (!this.isPersonalInfoEditing) {
+      personalInfoInputs.forEach(input => input.removeAttribute('readonly'));
+      personalInfoInputs[0]?.focus();
+    } else {
+      personalInfoInputs.forEach(input => input.setAttribute('readonly', 'true'));
+
+      const updatedData: Record<string, string> = {};
+      personalInfoInputs.forEach(input => {
+        updatedData[input.id] = input.value;
+      });
+      console.log('Dados atualizados:', updatedData);
+    }
+
+    this.isPersonalInfoEditing = !this.isPersonalInfoEditing;
+    this.personalInfoButtonLabel = this.isPersonalInfoEditing ? 'Salvar' : 'Editar';
+  }
+
+  toggleAddressInfo() {
+    const addressInfoInputs = document.querySelectorAll<HTMLInputElement>('.address-info-input');
+
+    if (!this.isAddressInfoEditing) {
+      addressInfoInputs.forEach(input => input.removeAttribute('readonly'));
+      addressInfoInputs[0]?.focus();
+    } else {
+      addressInfoInputs.forEach(input => input.setAttribute('readonly', 'true'));
+
+      const updatedData: Record<string, string> = {};
+      addressInfoInputs.forEach(input => {
+        updatedData[input.id] = input.value;
+      });
+      console.log('Dados atualizados:', updatedData);
+    }
+
+    this.isAddressInfoEditing = !this.isAddressInfoEditing;
+    this.addressInfoButtonLabel = this.isAddressInfoEditing ? 'Salvar' : 'Editar';
+  }
+  
+  
 }
